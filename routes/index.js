3077511400a65c2b979ab281a725a13c43fb32ef
Zack Hartmann <zackhartmann@gmail.com>
2018-04-30 13:30:16 -0400

    Added new View "SQLite"

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET sqlite page. */
+router.get('/sqlite', function(req, res, next) {
+  res.render('sqlite', { title: 'SQLite' });
+});
+
 module.exports = router;
