cbee362e299358799e3e807f0c310ecead407955
Zack Hartmann <zackhartmann@gmail.com>
2018-04-30 21:19:59 -0400

    Implemented Coins Route

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET sqlite page. */
-router.get('/sqlite', function(req, res, next) {
-  res.render('sqlite', { title: 'SQLite' });
-});
-
 module.exports = router;
