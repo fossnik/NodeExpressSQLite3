df14a8258a4f935781d0d11377a4e98c48845844
Zack Hartmann <zackhartmann@gmail.com>
2018-05-01 02:19:14 -0400

    Implemented Snapshot Detail View

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,41 @@ let router = express.Router();
 /* GET coins listing. */
 router.get('/', getAllCoins);
 router.get('/:currencyPair', coinSnapList);
+router.get('/:currencyPair/:snapId', coinSnapDetails);
+
+function coinSnapDetails(req, res, next) {
+	const sqlite3 = require('sqlite3').verbose();
+	const DB_PATH = 'coinsnapshot.db';
+
+	// access the database - create db object
+	let db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Connected to the Database.');
+	});
+
+	const coin = req.params.currencyPair;
+	const snapId = req.params.snapId;
+	const sql = `SELECT *
+	 			FROM ${coin}
+	 			WHERE ID=${snapId}`;
+
+	db.all(sql, [], (err, details) => {
+		if (err)
+			return next(err);
+
+		res.render('SnapshotDetail',{ details: details[0] });
+		console.log('Passed data to \'SnapshotDetail\' view');
+	});
+
+	db.close((err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Closed the Database.');
+	});
+}
 
 function coinSnapList(req, res, next) {
 	const sqlite3 = require('sqlite3').verbose();
@@ -17,14 +52,16 @@ function coinSnapList(req, res, next) {
 		console.log('Connected to the Database.');
 	});
 
+	const coin = req.params.currencyPair;
 	const sql = `SELECT dateCreated
-	 			FROM ${req.params.currencyPair}`;
+	 			FROM ${coin}
+	 			ORDER BY ID`;
 
 	db.all(sql, [], (err, snaps) => {
 		if (err)
 			return next(err);
 
-		res.render('SnapshotIndex', {snaps: snaps, title: "Index of Snapshots"});
+		res.render('SnapshotIndex', {snaps: snaps, coin: coin, title: "Index of Snapshots"});
 		console.log('Passed ' + snaps.length + ' snapshots to \'SnapshotIndex\' view');
 	});
 
