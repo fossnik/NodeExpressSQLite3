1b4ef92d5e111172be348f2fbd230d781e3859dc
Zack Hartmann <zackhartmann@gmail.com>
2018-05-01 01:14:39 -0400

    Implemented SnapshotIndex View

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ let router = express.Router();
 
 /* GET coins listing. */
 router.get('/', getAllCoins);
+router.get('/:currencyPair', coinSnapList);
 
-function getAllCoins(req, res, next) {
+function coinSnapList(req, res, next) {
 	const sqlite3 = require('sqlite3').verbose();
 	const DB_PATH = 'coinsnapshot.db';
 
@@ -13,7 +14,38 @@ function getAllCoins(req, res, next) {
 		if (err)
 			console.error(err.message);
 
-		console.log('Connected to the CoinDB Database.');
+		console.log('Connected to the Database.');
+	});
+
+	const sql = `SELECT dateCreated
+	 			FROM ${req.params.currencyPair}`;
+
+	db.all(sql, [], (err, snaps) => {
+		if (err)
+			return next(err);
+
+		res.render('SnapshotIndex', {snaps: snaps, title: "Index of Snapshots"});
+		console.log('Passed ' + snaps.length + ' snapshots to \'SnapshotIndex\' view');
+	});
+
+	db.close((err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Closed the Database.');
+	});
+}
+
+function getAllCoins(req, res, next) {
+	const sqlite3 = require('sqlite3').verbose();
+	const DB_PATH = 'coinsnapshot2.db';
+
+	// access the database - create db object
+	let db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Connected to the Database.');
 	});
 
 	// acquire list of table names (coins)
@@ -27,13 +59,14 @@ function getAllCoins(req, res, next) {
 
 		// render the list of all coins in the view 'CoinsIndex'
 		res.render('CoinsIndex', {coins: coins, title: "Index of Coins"});
+		console.log('Passed ' + coins.length + ' coins to \'CoinsIndex\' view');
 	});
 
 	db.close((err) => {
 		if (err)
 			console.error(err.message);
 
-		console.log('Closed the CoinDB Database.');
+		console.log('Closed the Database.');
 	});
 }
 
