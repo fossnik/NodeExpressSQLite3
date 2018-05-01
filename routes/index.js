ee3a8c4aec997a00819f6d55a955f22f4f06bd4a
Zack Hartmann <zackhartmann@gmail.com>
2018-04-30 23:14:58 -0400

    Refactored Coin Index into Separate View on the Index Route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET coins listing. */
+router.get('/', getAllCoins);
+
+function getAllCoins(req, res, next) {
+	const sqlite3 = require('sqlite3').verbose();
+	const DB_PATH = 'coinsnapshot.db';
+
+	// access the database - create db object
+	let db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Connected to the CoinDB Database.');
+	});
+
+	// acquire list of table names (coins)
+	let sql = `SELECT name
+			 FROM sqlite_master
+			 WHERE type='table'
+			 ORDER BY name`;
+	db.all(sql, [], (err, rows) => {
+		if (err)
+			return next(err);
+
+		res.render('CoinsIndex', {rows: rows, title: "Index of Coins"});
+	});
+
+	db.close((err) => {
+		if (err)
+			console.error(err.message);
+
+		console.log('Closed the CoinDB Database.');
+	});
+}
 
 module.exports = router;
